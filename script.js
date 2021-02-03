8b3057585f83acd9b7d84667ab307cd0aebd2746
Gabriel9Stresser <65667183+Gabriel9Stresser@users.noreply.github.com>
2021-02-03 18:55:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
+const score = document.querySelector('.score')
 let isJumping = false;
+let isGameOver = false;
 let position = 0;
+let pontos = 0;
 
 function handleKeyUp(event) {
-    if(event.keyCode === 32) {
+    if(event.keyCode === 38) {
         if (!isJumping) {
             jump();
         }
     }
+    if (event.keyCode === 13) {
+        reloadThePage()
+    }
 }
+
 
 function jump() {
     isJumping = true;
@@ -33,13 +40,13 @@ function jump() {
             position += 20;
             dino.style.bottom = position + 'px';
         }
-    }, 20);
+    }, 30);
 }
 
 function createCactus() {
     const cactus = document.createElement('div');
-    let cactusPosition = 1000;
-    let randomTime = Math.random() * 6000;
+    let cactusPosition = 1100;
+    let randomTime = Math.random() * (3000 - 500)+ 500;
 
     cactus.classList.add('cactus');
     cactus.style.left = 1000 + 'px';
@@ -49,17 +56,32 @@ function createCactus() {
         if(cactusPosition < -60) {
             clearInterval(leftInterval);
             background.removeChild(cactus);
-        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            pontos += 1;
+
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {            
             // Game Over
             clearInterval(leftInterval);
-            document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+            document.body.innerHTML = `<div class="game-over">
+            <h1>Fim de jogo, você fez ${pontos} pontos</br></br>
+            Aperte o Enter ou clique em Restart para reiniciar o jogo
+            </h1><div>
+            <div class="reload" onclick="reloadThePage()">Restart</div>
+            <div ><a class="reload" href="file:///C:/Program%20Files/Git/Dino-Game/telaInical.html" style="text-decoration:none">Menu</a></div>
+            </div></div>`;
         }else {
-            cactusPosition -= 10;
-            cactus.style.left = cactusPosition + 'px';
+            if (!isGameOver) {
+                score.innerHTML = "Score:  " + pontos
+                cactusPosition -= 10;
+                cactus.style.left = cactusPosition + 'px';
+            }
         }
-    }, 20);
+    }, 25);
 
     setTimeout(createCactus, randomTime);
+}
+
+function reloadThePage() {
+    window.location.reload();
 }
 
 createCactus();
